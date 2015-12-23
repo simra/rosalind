94f1891b95cbab6e75769a6e56deb8ab598a4215
simra <robert.sim@live.com>
2015-12-22 22:03:17 -0800

    nkew

--- trees/Program.fs
+++ trees/Program.fs
@@ -224,6 +224,152 @@ getData "ctbl"
 |> ctbl
 
 
+// NKEW.
+// So the question is do I refactor the newick code above or rewrite it here...
+// Refactoring was relatively fast and painless. Ideally we'd adjust parseWtTree to handle
+// unweighted or weighted trees.
+type WtNwckTree =
+    | WtLeaf of (float*string)
+    | WtAnon of float
+    | WtInternal of (float*string*WtNwckTree list)
+
+
+let rec parseWtTree (str:string) =
+    if str="" then raise (ex "Empty string")
+    else if str.[0]<>'(' then raise (ex "Expected open brace")
+    else
+        let findClose () =
+            let mutable o=0
+            let mutable i=1
+            while (i<str.Length && (str.[i]<>')' || o>0)) do
+                if str.[i]='(' then o<- o+1
+                else if str.[i]=')' then o<-o-1
+                i<-i+1
+            i
+        let getWt (s:string) =
+            s.Split(':')
+            |> fun toks -> 
+                if toks.Length<2 then
+                    eprintfn "Warning: no weight for string %s" s
+                    0.,s
+                else
+                    (float toks.[1],toks.[0])
+        let findLbl i = // TODO: I think we should never see close brace here.
+            if i+1>=str.Length || str.[i+1]=';' || str.[i+1]=')' || str.[i+1]=',' then ("",i+1)
+            else
+                let mutable j=i+2
+                while j<str.Length && str.[j]<>';' && str.[j]<>',' && str.[j]<>')' do j<-j+1
+                let start=i+1
+                let len=j-i-1
+                //eprintfn "%s %d %d %d" str str.Length start len                
+                (str.Substring(start,len),j+1)
+        let i = findClose ()
+        let (l,j) = findLbl i
+        let (wt,lbl)=getWt l
+        WtInternal(wt,lbl,parseWtList (str.Substring(1,i-1)))// :: parseTree (str.Substring(j))
+and parseWtList str =
+    let commaSep ()=
+        Seq.unfold(
+            fun (o,i,last) ->
+                if i>str.Length then None
+                else if i=str.Length then
+                    if o=0 then
+                        Some(Some(str.Substring(last+1)),(0,i+1,i))
+                    else
+                        raise (ex "Unexpected end of string")
+                else if o=0 && str.[i]=',' then
+                    Some(Some(str.Substring(last+1,i-last-1)),(0,i+1,i))
+                else if str.[i]='(' then 
+                    Some(None,(o+1,i+1,last))
+                else if str.[i]=')' then
+                    Some(None,(o-1,i+1,last))
+                else Some(None,(o,i+1,last))
+            ) (0,0,-1)
+        |> Seq.choose id
+    commaSep() 
+    |> Seq.map (fun s ->
+        //if s="" then Anon
+        if s.[0]=':' then WtAnon(float (s.Substring(1)))
+        else if s.[0]='(' then parseWtTree s
+        else WtLeaf(float (s.Split(':').[1]),s.Split(':').[0])
+    )
+    |> List.ofSeq
+
+let wtAdjacencies _nwk =
+    let d = new Dictionary<string,(float*string) list>()
+    let rec adj nwk : unit =        
+        let checkAnon s =
+            if s="" then 
+                let lbl=sprintf "anon_%d" (d.Count+1)
+                d.[lbl]<-List.empty
+                lbl
+            else 
+                if not (d.ContainsKey(s)) then d.[s]<-List.empty
+                s
+
+        match nwk with
+        | WtLeaf(w,s) -> ()
+        | WtAnon(w) -> ()
+        | WtInternal(w,l,n) ->
+            let l' = checkAnon l                
+            n
+            |> Seq.iter 
+                (fun nn ->
+                    match nn with
+                    | WtLeaf(w,s) -> 
+                        let s'=checkAnon s
+                        d.[s']<-(w,l')::d.[s']
+                        d.[l']<-(w,s')::d.[l']
+                    | WtAnon(w) ->
+                        let s'= checkAnon ""
+                        d.[s']<-(w,l')::d.[s']
+                        d.[l']<-(w,s')::d.[l']
+                    | WtInternal(w,s,sub) ->
+                        let s'=checkAnon s
+                        d.[s']<-(w,l')::d.[s']
+                        d.[l']<-(w,s')::d.[l']
+                        adj (WtInternal(w,s',sub))
+                
+                )   
+    adj _nwk
+    d
+// inefficient?  Proper handling of dead ends?
+let wtDistance (adj:Dictionary<string,(float*string) list>) a b =
+    let visited=new HashSet<string>()
+    let safeValue=float adj.Count*1e6;
+    let rec dist x =
+        //eprintfn "Visiting %s" x        
+        visited.Add(x)|> ignore        
+        if x=b then 0.
+        else 
+           // screams out for a computation expression     
+           adj.[x]
+           |> Seq.map                 
+                (fun (w,n) ->
+                    if not (visited.Contains(n)) then
+              //          eprintfn "Trying %s" n
+                        w + (dist n)
+                    else
+                        safeValue)
+           |> Seq.min 
+                     
+    dist a    
+
+getData "nkew"
+|> splitNewline
+|> Array.ofSeq
+|> fun s -> 
+        seq {
+            for i in [0..2..s.Length-1] do
+                yield (parseWtTree s.[i]|>wtAdjacencies,s.[i+1].Split(' '))
+            }
+//|> Seq.take 1
+|> Seq.map (fun (adj,toks) -> wtDistance adj toks.[0] toks.[1])
+|> Seq.map string
+|> String.concat " "
+|> printfn "%s"    
+
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
